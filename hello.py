267280c2a110df879ac43b0bc0fd6430a7e2ee4e
SeungWookHan <hswook12@me.com>
2020-10-24 22:22:20 +0900

    2f

--- hello.py
+++ hello.py
@@ -1,20 +1,19 @@
-from flask import Flask, redirect, abort
-from flask import request
+from flask import Flask
+from flask_script import Manager
 app = Flask(__name__)
+
+manager = Manager(app)
 
 
 @app.route('/')
 def index():
-    return redirect('http://www.naver.com')
+    return '<h1>Hello World!</h1>'
 
 
-@app.route('/user/<id>')
-def get_user(id):
-    user = load_user(id)
-    if not user:
-        abort(404)
-    return '<h1>Hello, %s</h1>' % user.name
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
