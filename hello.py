10b5ca409277286688f6467911922394f74e1267
SeungWookHan <hswook12@me.com>
2020-10-24 21:44:10 +0900

    2e

--- hello.py
+++ hello.py
@@ -1,13 +1,19 @@
-from flask import Flask, make_response
+from flask import Flask, redirect, abort
 from flask import request
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
+    return redirect('http://www.naver.com')
+
+
+@app.route('/user/<id>')
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+    return '<h1>Hello, %s</h1>' % user.name
 
 
 if __name__ == '__main__':
