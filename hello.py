f5267e9d142efad66d33553c791076475a45ae13
SeungWookHan <hswook12@me.com>
2020-10-26 22:35:48 +0900

    3a

--- hello.py
+++ hello.py
@@ -1,19 +1,15 @@
-from flask import Flask
-from flask_script import Manager
-app = Flask(__name__)
+from flask import Flask, render_template
 
-manager = Manager(app)
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
-
-if __name__ == '__main__':
-    manager.run()
+app.run()
